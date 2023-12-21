23ff46b31fc00f253beda1268cdb68139db0cd79
George <geosem042@gmail.com>
2023-12-21 22:38:27 +0200

    Added checkbox components

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,35 +1,36 @@
 import { useState, useEffect } from "react";
+import CheckboxAll from "./partials/CheckboxAll";
+import CheckboxItem from "./partials/CheckboxItem";
 
 function TaskList() {
   const [tasks, setTasks] = useState([
-    { id: 1, title: 'Refactor codebase' },
-    { id: 2, title: 'Write unit tests' },
-    { id: 3, title: "Fix bug in login flow" },
-    { id: 4, title: "Write documentation" },
-    { id: 5, title: "Implement new feature" },
-    { id: 6, title: "Review pull requests" },
-    { id: 7, title: "Optimize performance" },
-    { id: 8, title: "Improve UI/UX design" },
-    { id: 9, title: "Conduct code audit" },
-    { id: 10, title: "Prepare deployment" },
+    { id: 1, title: "Refactor codebase", checked: false },
+    { id: 2, title: 'Write unit tests', checked: false },
+    { id: 3, title: "Fix bug in login flow", checked: false },
+    { id: 4, title: "Write documentation", checked: false },
+    { id: 5, title: "Implement new feature", checked: false },
+    { id: 6, title: "Review pull requests", checked: false },
+    { id: 7, title: "Optimize performance", checked: false },
+    { id: 8, title: "Improve UI/UX design", checked: false },
+    { id: 9, title: "Conduct code audit", checked: false },
+    { id: 10, title: "Prepare deployment", checked: false },
   ]);
+
+  const handleCheck = (id) => {
+    const newTasks = tasks.map(task => task.id === id ? { ...task, checked: !task.checked } : task);
+    setTasks(newTasks);
+    
+    const checkedTask = newTasks.find(task => task.id === id);
+    console.log(`Task ${checkedTask.id}: ${checkedTask.title} checked status is now ${checkedTask.checked}`);
+  };
+
   return (
     <div className="mt-5 shadow-sm border rounded-lg overflow-x-auto" id="body">
       <table className="w-full table-auto text-sm text-left">
         <thead className="text-gray-600 font-medium border-b">
           <tr>
             <th className="py-3 px-6 flex items-center gap-x-4">
-              <div>
-                <input
-                  type="checkbox"
-                  id="checkbox-all-items"
-                  className="checkbox-item peer hidden"
-                />
-                <label
-                  htmlFor="checkbox-all-items"
-                  className="relative flex w-5 h-5 bg-white peer-checked:bg-indigo-600 rounded-md border ring-offset-2 ring-indigo-600 duration-150 peer-active:ring cursor-pointer after:absolute after:inset-x-0 after:top-[3px] after:m-auto after:w-1.5 after:h-2.5 after:border-r-2 after:border-b-2 after:border-white after:rotate-45"
-                ></label>
-              </div>
+              <CheckboxAll tasks={tasks} setTasks={setTasks} />
               Tasks
             </th>
             <th className="py-3 px-6"></th>
@@ -39,32 +40,14 @@ function TaskList() {
           {tasks.map(task => (
             <tr key={task.id} className="odd:bg-gray-50 even:bg-white">
             <td className="px-6 py-4 whitespace-nowrap flex items-center gap-x-4">
-              <div>
-                <input
-                  type="checkbox"
-                  id={`checkbox-${task.id}`}
-                  name={`checkbox-${task.id}`}
-                  className="checkbox-item peer hidden"
-                />
-
-                <label
-                    htmlFor={`checkbox-${task.id}`}
-                  className="relative flex w-5 h-5 bg-white peer-checked:bg-indigo-600 rounded-md border ring-offset-2 ring-indigo-600 duration-150 peer-active:ring cursor-pointer after:absolute after:inset-x-0 after:top-[3px] after:m-auto after:w-1.5 after:h-2.5 after:border-r-2 after:border-b-2 after:border-white after:rotate-45"
-                ></label>
-              </div>
-                {task.title}
+              <CheckboxItem task={task} onCheck={() => handleCheck(task.id)} />
+              {task.title}
             </td>
             <td className="text-right px-6 whitespace-nowrap">
-              <a
-                href="#"
-                className="py-2 px-3 font-medium text-indigo-600 hover:text-indigo-500 duration-150 hover:bg-gray-50 rounded-lg"
-              >
+              <a href="#" className="py-2 px-3 font-medium text-indigo-600 hover:text-indigo-500 duration-150 hover:bg-gray-50 rounded-lg">
                 Edit
               </a>
-              <button
-                href="#"
-                className="py-2 leading-none px-3 font-medium text-red-600 hover:text-red-500 duration-150 hover:bg-gray-50 rounded-lg"
-              >
+              <button href="#" className="py-2 leading-none px-3 font-medium text-red-600 hover:text-red-500 duration-150 hover:bg-gray-50 rounded-lg">
                 Delete
               </button>
             </td>
